792a2bf581415bb90e91422216baa0d8c1f311bf
Nishil Patel <nishil.k.patel@gmail.com>
2026-02-17 10:04:11 +0530

    Add files via upload

---
+++ AriesFont5x7.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <Arduino.h>
+
+#define ARIES_FONT_WIDTH   5
+#define ARIES_FONT_HEIGHT  7
+#define ARIES_FONT_FIRST   32
+
+extern const uint8_t System5x7[];
+
+void AriesDrawChar(
+    class AriesDMD& dmd,
+    int x, int y,
+    char c,
+    uint8_t mode = 0
+);
+
+void AriesDrawString(
+    class AriesDMD& dmd,
+    int x, int y,
+    const char* str,
+    uint8_t mode = 0
+);
